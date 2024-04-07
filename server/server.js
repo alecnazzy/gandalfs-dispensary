9a9f79f71df9f7ce3f6132fdd1072e4bf29d5135
Alec Nazworth <nazworthalec@outlook.com>
2024-04-07 14:22:58 -0400

    -enabled cors on server

--- server/server.js
+++ server/server.js
@@ -1,5 +1,13 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+
+// enabling CORS for some specific origins only.
+let corsOptions = {
+  origin: ["https://gandalfs-pipeweed-dispensary.onrender.com"],
+};
+
+app.use(cors(corsOptions));
 
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from API server!" });
