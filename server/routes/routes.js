1785d9aae90f93351c9644fe50d48694af2460d5
Alec Nazworth <nazworthalec@outlook.com>
2024-04-15 18:25:46 -0400

    -retrying last commit

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -31,20 +31,4 @@ router.get("/products/:id/" + apiKey, async (req, res) => {
   }
 });
 
-// POST
-// router.post("/post", async (req, res) => {
-//   const data = new Products({
-//     name: req.body.name,
-//     price: req.body.price,
-//     image: req.body.image,
-//   });
-
-//   try {
-//     const dataToSave = await data.save();
-//     res.status(200).json(dataToSave);
-//   } catch (error) {
-//     res.status(400).json({ message: error.message });
-//   }
-// });
-
 module.exports = router;
